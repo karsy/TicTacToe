d2aec84d553eef244c82ebff39a3e88a90304869
karsy <vegard.karstang@gmail.com>
2016-04-08 14:38:52 +0200

    Started work on the horizontal test.

--- test/tictactoe/BoardMovesTest.java
+++ test/tictactoe/BoardMovesTest.java
@@ -10,7 +10,43 @@ import static org.junit.Assert.*;
 public class BoardMovesTest {
 
 	@Test
-	public void testGetMove() throws Exception {
+	public void testGetBlockingHorizontal() throws Exception {
+		Board board = setupBoard(new String[]{"x", "x", " ", "o", " ", " ", " ", " ", " "});
+		Move move = BoardMoves.getBlockingMove(board, new Move(1, 0, "x"), "o");
+		assertNotNull(move);
+		assertEquals(move.x, 2);
+		assertEquals(move.y, 0);
 
+		board = setupBoard(new String[]{"x", "x", "o", " ", " ", " ", " ", " ", " "});
+		Move move2 = BoardMoves.getBlockingMove(board, new Move(1, 0, "x"), "o");
+		assertNull(move2);
+	}
+
+	@Test
+	public void testGetBlockingVertical() throws Exception {
+
+	}
+
+	@Test
+	public void testGetBlockingTopLeftDiagonal() throws Exception {
+
+	}
+
+	@Test
+	public void testGetBlockingBottomLeftDiagonal() throws Exception {
+
+	}
+
+	@Test
+	public void testGetWinningMove() throws Exception {
+
+	}
+
+	private Board setupBoard(String[] values) {
+		Board board = new Board();
+		for (int i = 0; i < values.length; i++) {
+			board.setValue(values[i], i % 3, (int) Math.floor(i / 3.0));
+		}
+		return board;
 	}
 }
